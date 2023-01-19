6bfa2d39be8723745f7e82d08ee8ff942b63abae
Minhvu Nguyen <manguye@ad.unc.edu>
2023-01-19 15:17:00 -0500

    Added if-else for values of match; need to count

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -6,14 +6,27 @@ __author__ = "730411646"
 chardle_word: str = input("Enter a 5-character word: ")
 chardle_character: str = input("Enter a single character: ")
 print("Searching for " + chardle_character + " in " + chardle_word)
+chardle_match: int = 0
 
 if (chardle_character == chardle_word[0]):
     print(chardle_character + " found at index 0 ")
+    chardle_match == int(chardle_match + 1)
 if (chardle_character == chardle_word[1]):
     print(chardle_character + " found at index 1 ")
+    chardle_match == int(chardle_match + 1)
 if (chardle_character == chardle_word[2]):
     print(chardle_character + " found at index 2 ")
+    chardle_match == int(chardle_match + 1)
 if (chardle_character == chardle_word[3]):
     print(chardle_character + " found at index 3 ")
+    chardle_match == int(chardle_match + 1)
 if (chardle_character == chardle_word[4]):
     print(chardle_character + " found at index 4 ")
+    chardle_match == int(chardle_match + 1)
+
+if (chardle_match == 0):
+    print("No instances of " + chardle_character + " found in " + chardle_word)
+if (chardle_match == 1):
+    print(chardle_match + " instance of " + chardle_character + " found in " + chardle_word)
+if (chardle_match >= 2):
+    print(chardle_match + " instances of " + chardle_character + " found in " + chardle_word)
