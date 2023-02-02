c446a905ff8abc4ce5d5832664f77fff85ad9ff9
Minhvu Nguyen <manguye@ad.unc.edu>
2023-02-02 14:38:56 -0500

    Conditional corrected lines 9-13

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -6,8 +6,8 @@ secret_word: str = "python"
 guess: str = input("What is your 6-letter guess? ")
 while len(guess) != 6:
     guess: str = input("That was not 6 letters! Try again: ")
-    if len(guess) == 6:    
-        if (guess == secret_word):
-            print("Woo! You got it!")
-        else:
-            print("Not quite! Play again soon!")
+if len(guess) == 6:    
+    if (guess == secret_word):
+        print("Woo! You got it!")
+    else:
+        print("Not quite! Play again soon!")
