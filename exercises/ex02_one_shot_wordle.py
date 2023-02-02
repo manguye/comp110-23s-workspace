d13d32c73144e73038354507aa4918bf0b9d398a
Minhvu Nguyen <manguye@ad.unc.edu>
2023-02-02 14:24:39 -0500

    Corrections to conditional statement line 9

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,11 +2,12 @@
 
 __author__ = "730411646"
 
-secret_word: str("python")
+secret_word: str = "python"
 guess: str = input("What is your 6-letter guess? ")
 while len(guess) != 6:
     guess: str = input("That was not 6 letters! Try again: ")
-    if (guess == secret_word):
-        print("Woo! You got it!")
-    else:
-        print("Not quite! Play again soon!")
+    if len(guess) == 6:    
+        if (guess == secret_word):
+            print("Woo! You got it!")
+        else:
+            print("Not quite! Play again soon!")
