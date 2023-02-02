6c79ad22933594233a647a6cf387bf5e578137ab
Minhvu Nguyen <manguye@ad.unc.edu>
2023-02-02 14:11:12 -0500

    Correction to loop (number of characters).

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -4,5 +4,5 @@ __author__ = "730411646"
 
 secret_word: str("python")
 guess: str = input("What is your 6-letter guess? ")
-while len(guess) != 0:
+while len(guess) != 6:
     print("That was not 6 letters! Try again: " + guess)
