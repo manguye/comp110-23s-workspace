8f816f751d5f3a3513918df42048b9811a39dd99
Minhvu Nguyen <manguye@ad.unc.edu>
2023-02-02 14:08:05 -0500

    Constructed test while statement.

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -4,3 +4,5 @@ __author__ = "730411646"
 
 secret_word: str("python")
 guess: str = input("What is your 6-letter guess? ")
+while len(guess) != 0:
+    print("That was not 6 letters! Try again: " + guess)
