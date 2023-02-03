2590bde59e770f630886e01362f29a1157da243e
Minhvu Nguyen <manguye@ad.unc.edu>
2023-02-03 08:48:20 -0500

    Added 2/3 character_checker code

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -13,7 +13,14 @@ character_checker: str = ""
 guess: str = input(f"What is your {len(secret_word)}-letter guess? ")
 while len(guess) != len(secret_word):
     guess: str = input(f"That was not {len(secret_word)} letters! Try again: ")
-if len(guess) == len(secret_word):    
+if len(guess) == len(secret_word):
+    while idx < len(secret_word):
+        if guess[idx] == secret_word[idx]:
+            character_checker = character_checker + GREEN_BOX
+        else:
+            character_checker = character_checker + WHITE_BOX
+        idx = idx + 1    
+    print(character_checker)
     if (guess == secret_word):
         print("Woo! You got it!")
     else:
