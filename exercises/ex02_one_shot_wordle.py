01cb1e3c1bd670dcec75345d25c69335d1f02aac
Minhvu Nguyen <manguye@ad.unc.edu>
2023-02-03 10:37:21 -0500

    Renamed variable guess to wordle-guess

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -9,19 +9,19 @@ YELLOW_BOX: str = "\U0001F7E8"
 secret_word: str = "python"
 idx: int = 0
 character_checker: str = ""
-guess: str = input(f"What is your {len(secret_word)}-letter guess? ")
+wordle_guess: str = input(f"What is your {len(secret_word)}-letter guess? ")
 
-while len(guess) != len(secret_word):
-    guess: str = input(f"That was not {len(secret_word)} letters! Try again: ")
-if len(guess) == len(secret_word):
+while len(wordle_guess) != len(secret_word):
+    wordle_guess: str = input(f"That was not {len(secret_word)} letters! Try again: ")
+if len(wordle_guess) == len(secret_word):
     while idx < len(secret_word):
-        if guess[idx] == secret_word[idx]:
+        if wordle_guess[idx] == secret_word[idx]:
             character_checker = character_checker + GREEN_BOX
         else:
             right_character_wrong_place: bool = False
             right_character_wrong_place_idx: int = 0
             while not right_character_wrong_place and right_character_wrong_place_idx < len(secret_word):
-                if guess[idx] == secret_word[right_character_wrong_place_idx]:
+                if wordle_guess[idx] == secret_word[right_character_wrong_place_idx]:
                     right_character_wrong_place = True
                 else:
                     right_character_wrong_place_idx = right_character_wrong_place_idx + 1
@@ -31,7 +31,7 @@ if len(guess) == len(secret_word):
                 character_checker = character_checker + WHITE_BOX
         idx = idx + 1    
     print(character_checker)
-    if (guess == secret_word):
+    if (wordle_guess == secret_word):
         print("Woo! You got it!")
     else:
         print("Not quite! Play again soon!")
