9bb5dfa8cc1f34c002ebacee7c55e8f6c9d361f0
Minhvu Nguyen <manguye@ad.unc.edu>
2023-02-02 19:49:47 -0500

    Added hint and index variables

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -7,6 +7,9 @@ GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
 secret_word: str = "python"
+idx: int = 0
+hint: str = ""
+
 guess: str = input(f"What is your {len(secret_word)}-letter guess? ")
 while len(guess) != len(secret_word):
     guess: str = input(f"That was not {len(secret_word)} letters! Try again: ")
