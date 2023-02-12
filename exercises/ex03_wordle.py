bea49031715dc86278f0da9d01065a8fd05197e2
Minhvu Nguyen <manguye@ad.unc.edu>
2023-02-12 16:10:08 -0500

    Function contains_char defined

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,3 +1,18 @@
 """EX03 - Wordle but with more turns."""
 
 __author__: "730411646"
+
+def contains_char(secret_word: str, character_check: str) -> bool:
+    """Checks to see if a given character is in the secret word."""
+    assert len(character_check) == 1
+    idx: int = 0
+    character_contained: bool = False
+    while not character_contained and idx < len(secret_word):
+        if character_check == secret_word[idx]:
+            character_contained = True
+        else:
+            idx = idx + 1 
+    if character_contained:
+        return True
+    else:
+        return False
