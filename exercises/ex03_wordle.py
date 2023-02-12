4da4bde875716ddfd5a376f4a598400b8824b74c
Minhvu Nguyen <manguye@ad.unc.edu>
2023-02-12 16:48:26 -0500

    Renamed idx in contains_char to character_idx

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -5,13 +5,13 @@ __author__: "730411646"
 def contains_char(secret_word: str, character_check: str) -> bool:
     """Checks to see if a given character is in the secret word."""
     assert len(character_check) == 1
-    idx: int = 0
+    character_idx: int = 0
     character_contained: bool = False
-    while not character_contained and idx < len(secret_word):
-        if character_check == secret_word[idx]:
+    while not character_contained and character_idx < len(secret_word):
+        if character_check == secret_word[character_idx]:
             character_contained = True
         else:
-            idx = idx + 1 
+            character_idx = character_idx + 1 
     if character_contained:
         return True
     else:
