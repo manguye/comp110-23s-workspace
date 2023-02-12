8aa2eb9d7a96649ebc8450b671991dd8d5f473cf
Minhvu Nguyen <manguye@ad.unc.edu>
2023-02-12 17:52:00 -0500

    Formatting corrections

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,6 +1,7 @@
 """EX03 - Wordle but with more turns."""
 
-__author__: "730411646"
+__author__ = "730411646"
+
 
 def contains_char(secret_word: str, character_check: str) -> bool:
     """Checks to see if a given character is in the secret word."""
@@ -16,6 +17,7 @@ def contains_char(secret_word: str, character_check: str) -> bool:
         return True
     else:
         return False
+
 
 def emojified(guess_word: str, secret_word: str) -> str:
     """Displays reponse regarding correctness of guess word."""
@@ -37,12 +39,14 @@ def emojified(guess_word: str, secret_word: str) -> str:
         idx = idx + 1
     return response
 
+
 def input_guess(word_length: int) -> str:
     """Checks to see if a given guess is the required length."""
     input_word: str = input(f"Enter a {word_length} character word: ")
     while len(input_word) != word_length:
         input_word = input(f"That wasn't {word_length} chars! Try again: ")
     return input_word
+
 
 def main() -> None:
     """The entrypoint of the program and main game loop."""
@@ -60,6 +64,7 @@ def main() -> None:
             turn_counter = turn_counter + 1
     if not game_won:
         print("X/6 - Sorry, try again tomorrow!")
+
 
 if __name__ == "__main__":
     main()
