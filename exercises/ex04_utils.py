654523ac8a45286446456cadb0588f5fe9d801e5
Minhvu Nguyen <manguye@ad.unc.edu>
2023-02-23 16:06:03 -0500

    Added Part 2 to Exercise 4

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,10 +1,21 @@
 def all(xs: list[int], x: int) -> bool:
     """Given a list of integers and an integer, checks if they are all the same."""
-    i: int = 0
     if len(xs) == 0:
-        return False
+        return False  
+    i: int = 0
     while i <= len(xs) - 1:
         if xs[i] != x:
             return False
         i += 1
     return True
+
+def max(input: list[int]) -> int:
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+    i: int = 0
+    x: int = 0
+    while i <= len(input) - 1:
+        if input[i] > x:
+            x = input[i]
+        i += 1
+    return x
