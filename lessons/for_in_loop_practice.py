f06af06ab40e60e81f5e8b1538701d1db033a53a
Minhvu Nguyen <manguye@ad.unc.edu>
2023-03-02 14:36:02 -0500

    Docstring explanation with range in for in loop

--- lessons/for_in_loop_practice.py
+++ lessons/for_in_loop_practice.py
@@ -4,6 +4,7 @@ pets: list[str] = ["Louie", "Bo", "Bear"]
 for animal in pets:
     print(f"Good boy, {animal}!")
 
+"""Added range practice with a list."""
 names: list[str] = ["Alyssa", "Janet", "Vrinda"]
 for idx in range(0, 3):
     print(f"{idx}: {names[idx]}")
