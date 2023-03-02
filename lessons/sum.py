876f957a179848c124ae78fa6d42c8b6891c6866
Minhvu Nguyen <manguye@ad.unc.edu>
2023-03-02 14:49:22 -0500

    Modified with for in loops and range

--- lessons/sum.py
+++ lessons/sum.py
@@ -1,10 +1,10 @@
-"""Example function for unit tests."""
+"""Example function for unit tests with for ... in ... loops and range."""
 
 def sum(xs: list[float]) -> float:
     """Return sum of all elements in xs"""
     sum_total: float = 0.0
     i: int = 0
-    while i < len(xs):
+    for i in range(0, len(xs)):
         sum_total += xs[i]
         i += 1
     return sum_total
