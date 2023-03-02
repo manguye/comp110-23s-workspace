a088576bf293c4d3456455734a108653f6dd2354
Minhvu Nguyen <manguye@ad.unc.edu>
2023-03-02 14:35:10 -0500

    Added in range practice

--- lessons/for_in_loop_practice.py
+++ lessons/for_in_loop_practice.py
@@ -1,5 +1,9 @@
 """Practicing for ...in ... loops."""
 
 pets: list[str] = ["Louie", "Bo", "Bear"]
-for name in pets:
-    print(f"Good boy, {name}!")
+for animal in pets:
+    print(f"Good boy, {animal}!")
+
+names: list[str] = ["Alyssa", "Janet", "Vrinda"]
+for idx in range(0, 3):
+    print(f"{idx}: {names[idx]}")
