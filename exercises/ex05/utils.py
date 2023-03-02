b4673663765285c5da695dd1df6df3b4f473afff
Minhvu Nguyen <manguye@ad.unc.edu>
2023-03-02 15:13:46 -0500

    Added part 2; unsure of correctness

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -10,3 +10,13 @@ def only_evens(xs: list[int]) -> list[int]:
         if number % 2 == 0:
             ys.append(number)
     return ys
+
+
+def concat(xs: list[int], ys: list[int]) -> list[int]:
+    """Given two lists of integers, combines them by appending the second to the end of the first."""
+    zs: list[int] = list()
+    for number in xs:
+        zs.append(number)
+    for number in ys:
+        zs.append(number)
+    return zs
