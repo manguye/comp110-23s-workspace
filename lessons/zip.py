d578e532f4218c9b530188300e23723f235aef3e
Minhvu Nguyen <manguye@ad.unc.edu>
2023-03-09 14:36:47 -0500

    Corrected zs to begin as empty

--- lessons/zip.py
+++ lessons/zip.py
@@ -4,7 +4,7 @@ __author__ = "730411646"
 
 def zip(xs: list[str], ys: list[int]) -> dict[str, int]:
     """Should produce a list where keys are strings and values are integers."""
-    zs: dict[str, int]
+    zs: dict[str, int] = {}
     i: int = 0
     if len(xs) != len(ys) or len(xs) == 0 or len(ys) == 0:
         return zs
