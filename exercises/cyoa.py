181be2bafeef48aa8db18b1378eb319a5f6d4993
Minhvu Nguyen <manguye@ad.unc.edu>
2023-03-09 14:53:05 -0500

    First steps in CYOA.

---
+++ exercises/cyoa.py
@@ -0,0 +1,14 @@
+"""EX06 - Choose Your Own Adventure."""
+
+__author__ = "730411646"
+
+
+points: int = 0
+player: str = input("What is your name? ")
+
+def main() -> None:
+    "The beginning."
+    global points
+
+if __name__ == "__main__":
+    main()
