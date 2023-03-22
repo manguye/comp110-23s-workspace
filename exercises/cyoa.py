591bc82325811ab5518cd2cc8051310b7497ddb9
Minhvu Nguyen <manguye@ad.unc.edu>
2023-03-22 03:18:51 -0400

    Added second option; gamebreaking bug not fixed

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -23,7 +23,13 @@ def main() -> None:
                     ct_idx = int(input("That's not an index value of one of the conventional toppings! Pick again! "))
             points += randint(1,10)
             print(f"Thanks to adding {conventional_toppings[ct_idx]}, your review score is now {points}! ")
-            player_choice = input("Would you like to add another 'conventional' topping, something 'unusual', or 'present' your pizza? ")
+            player_choice = input(f"Would you like to add another 'conventional' topping, something 'unusual', or 'present' your pizza, Chef {player}? ")
+        if player_choice == "unusual":
+            unusual_topping: str = input (f"What do you plan on adding, Chef {player}? ")
+            unusuality: int = unusual_topping_function(len(unusual_topping), points)
+            points += unusuality
+            print(f"Thanks to adding {unusual_topping}, your review score is now {points}! ")
+            player_choice = input(f"Chef {player}, would you like to add anything else 'unusual', add something 'conventional', or 'present' your pizza? ")
     print(f"Your pizza got a review score of {points}!")
 
 def greet() -> None:
@@ -31,6 +37,15 @@ def greet() -> None:
     global player
     print("Pizzamaking is no easy feat, but you're here to wow the crowd with your exceptional ideas. You've come to an international pizzamaking competition to make a combination that'll impress the judges. Get as high a review score as you can and aim for first place!")
     player = input("What is your name, chef? ")
+
+def unusual_topping_function(x: int, y: int) -> int:
+    """Given a custom topping, awards or detracts randomly calculated score based on if the points value is even or odd."""
+    if y % 2 == 0:
+        x = x * randint(1, 10)
+        return x
+    else:
+        x = x * randint(-10, -1)
+        return x
 
 if __name__ == "__main__":
     main()
