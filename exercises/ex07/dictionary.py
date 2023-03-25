6d7c9b54c0011d321d57edf000d6543014e95a50
Minhvu Nguyen <manguye@ad.unc.edu>
2023-03-25 17:21:51 -0400

    Fresh start; part 2 difficult

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -13,17 +13,8 @@ def invert(xs: dict[str, str]) -> dict[str, str]:
 
 def favorite_color(xs: dict[str, str]) -> str:
     """Given a dictionary input, should return the value that appears the most. If there is a tie, returns the value that appeared first."""
-    y: str = ""
-    ys: dict[str, int] = ()
-    for x in xs:
-        ys[x] += 1
-    while len(ys) > 1:
-        if ys(1) < ys(0):
-            ys.pop(1)
-    y = ys[0]
-    return y
 
-
+print(favorite_color({"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}))
 
 def count(xs: list[str]) -> dict[str, int]:
     """Given a list of strings, should return a dictionary where each key is a unique value from the list and each value is the number of times that key appeared in the input list."""
