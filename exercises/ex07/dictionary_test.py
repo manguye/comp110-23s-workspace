1b7a43703a24221b235dbba67b9449fba7e7641c
Minhvu Nguyen <manguye@ad.unc.edu>
2023-03-25 17:23:35 -0400

    Modified criteria on single pair invert

--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -7,8 +7,8 @@ from exercises.ex07.dictionary import invert, favorite_color, count, pytest
 
 def test_single_pair_invert() -> None:
     """Tests invert if input is a single pair dictionary."""
-    test_dict: dict[str, str] = {"a", "z"}
-    assert invert(test_dict) == {"z", "a"}
+    test_dict: dict[str, str] = {"apple", "cat"}
+    assert invert(test_dict) == {"cat", "apple"}
 
 with pytest.raises(KeyError):
     my_dictionary = {'kris': 'jordan', 'michael': 'jordan'}
