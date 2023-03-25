b01e895958b6fc38384c91516d1e4c47a93c3ff1
Minhvu Nguyen <manguye@ad.unc.edu>
2023-03-25 17:04:19 -0400

    Added option test for KeyError

--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -3,9 +3,13 @@
 __author__ = "730411646"
 
 
-from exercises.ex07.dictionary import invert, favorite_color, count
+from exercises.ex07.dictionary import invert, favorite_color, count, pytest
 
 def test_single_pair_invert() -> None:
     """Tests invert if input is a single pair dictionary."""
     test_dict: dict[str, str] = {"a", "z"}
     assert invert(test_dict) == {"z", "a"}
+
+with pytest.raises(KeyError):
+    my_dictionary = {'kris': 'jordan', 'michael': 'jordan'}
+    invert(my_dictionary)
