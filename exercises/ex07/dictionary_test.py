2f7475151d12a793b625727d2223f0b025963c73
Minhvu Nguyen <manguye@ad.unc.edu>
2023-03-29 17:55:33 -0400

    imported pytest properly

--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -3,7 +3,8 @@
 __author__ = "730411646"
 
 
-from exercises.ex07.dictionary import invert, favorite_color, count, pytest
+from exercises.ex07.dictionary import invert, favorite_color, count
+import pytest
 
 def test_single_pair_invert() -> None:
     """Tests invert if input is a single pair dictionary."""
