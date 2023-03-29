df4cf3678ac78d99f4f0ae2bda1359afd2752ffb
Minhvu Nguyen <manguye@ad.unc.edu>
2023-03-29 17:53:55 -0400

    added test to invert

--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -7,8 +7,13 @@ from exercises.ex07.dictionary import invert, favorite_color, count, pytest
 
 def test_single_pair_invert() -> None:
     """Tests invert if input is a single pair dictionary."""
-    test_dict: dict[str, str] = {"apple", "cat"}
-    assert invert(test_dict) == {"cat", "apple"}
+    test_dict: dict[str, str] = {"apple": "cat"}
+    assert invert(test_dict) == {"cat": "apple"}
+
+def test_multiple_pairs_invert() -> None:
+    """Tests invert if input is a dictionary with multiple pairs."""
+    test_dict: dict[str, str] = {"a": "z", "b": "y", "c": "x"}
+    assert invert(test_dict) == {"z": "a", "y": "b", "x": "c"}
 
 with pytest.raises(KeyError):
     my_dictionary = {'kris': 'jordan', 'michael': 'jordan'}
