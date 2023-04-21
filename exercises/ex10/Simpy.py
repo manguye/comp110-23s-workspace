80caec0088f459411b1c818cdedb211c8ccb74fc
Minhvu Nguyen <manguye@ad.unc.edu>
2023-04-21 15:13:33 -0400

    Imperative mood

--- exercises/ex10/Simpy.py
+++ exercises/ex10/Simpy.py
@@ -13,15 +13,15 @@ class Simpy:
     values: list[float]
 
     def __init__(self, number_list: list[float]):
-        """Initializes a new Simpy object."""
+        """Initialize a new Simpy object."""
         self.values = number_list
 
     def __str__(self) -> str:
-        """Makes printing Simpy prettier."""
+        """Make printing Simpy prettier."""
         return f"Simpy({self.values})"
     
     def fill(self, number: float, how_much: int) -> None:
-        """Fills Simpy.values with a specific number of repeating values."""
+        """Fill Simpy.values with a specific number of repeating values."""
         new_values: list[float] = []
         while how_much > 0:
             new_values.append(number)
@@ -29,7 +29,7 @@ class Simpy:
         self.values = new_values
 
     def arange(self, start: float, stop: float, step: float = 1.0) -> None:
-        """Fills in Simpy.values with range of values in terms of floats."""
+        """Fill in Simpy.values with range of values in terms of floats."""
         assert step != 0
         number: float = start
         while number != stop:
@@ -37,12 +37,12 @@ class Simpy:
             number += step
 
     def sum(self) -> float:
-        """Sums the floats in Simpy.values through delegation."""
+        """Sum the floats in Simpy.values through delegation."""
         final_value: float = sum(self.values)
         return final_value
     
     def __add__(self, rhs: Union[float, Simpy]) -> Simpy:
-        """Overrides add to allow addition between Simpy objects and/or floats."""
+        """Override add to allow addition between Simpy objects and/or floats."""
         new_simpy: Simpy = Simpy([])
         if isinstance(rhs, float):
             for x in range(0, len(self.values)):
@@ -54,7 +54,7 @@ class Simpy:
         return new_simpy
     
     def __pow__(self, rhs: Union[float, Simpy]) -> Simpy:
-        """Overrides power to allow exponentiation between Simpy objects and/or floats."""
+        """Override power to allow exponentiation between Simpy objects and/or floats."""
         new_simpy: Simpy = Simpy([])
         if isinstance(rhs, float):
             for x in range(0, len(self.values)):
@@ -66,7 +66,7 @@ class Simpy:
         return new_simpy
     
     def __eq__(self, rhs: Union[float, Simpy]) -> list[bool]:
-        """Returns a list of bools that filters Simpy objects if they match other Simpy objects or floats."""
+        """Return a list of bools that filters Simpy objects if they match other Simpy objects or floats."""
         checklist: list[bool] = []
         if isinstance(rhs, float):
             for x in range(0, len(self.values)):
@@ -84,7 +84,7 @@ class Simpy:
         return checklist
     
     def __gt__(self, rhs: Union[float, Simpy]) -> list[bool]:
-        """Returns a list of bools that filters Simpy objects if they are greater than other Simpy objects or floats."""
+        """Return a list of bools that filters Simpy objects if they are greater than other Simpy objects or floats."""
         checklist: list[bool] = []
         if isinstance(rhs, float):
             for x in range(0, len(self.values)):
@@ -102,7 +102,7 @@ class Simpy:
         return checklist
     
     def __getitem__(self, rhs: Union[int, list[bool]]) -> Union[float, Simpy]:
-        """Allows the user to read specific items from the Simpy array."""
+        """Allow the user to read specific items from the Simpy array."""
         if isinstance(rhs, int):
             requested_number: float = self.values[rhs]
             return requested_number
